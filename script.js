600226a26659cdde6acf3acf97915a4131fb06d0
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2023-02-08 10:04:32 -0300

    Create a function that plays a round with the player e computer choices

--- script.js
+++ script.js
@@ -3,3 +3,34 @@ function getComputerChoice() {
     const random = Math.floor(Math.random() * choice.length);
     return choice[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+
+    if (playerSelection === "ROCK") {
+        if (computerSelection === "PAPER") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection === 'SCISSOR') {
+            return "You win! Rock beats Scissor";
+        } else {
+            return "It's a draw!";
+        }
+    } else if (playerSelection === "PAPER") {
+        if (computerSelection === "SCISSOR") {
+            return "You Lose! Scissor beats Paper";
+        } else if (computerSelection === "ROCK") {
+            return "You win! Paper beats Rock";
+        } else {
+            return "It's a draw!";
+        }
+    } else {
+        if (computerSelection === "ROCK") {
+            return "You Lose! Rock beats Scissor";
+        } else if (computerSelection === "PAPER") {
+            return "You win! Scissor beats Paper";
+        } else {
+            return "It's a draw!";
+        }
+    }
+}
