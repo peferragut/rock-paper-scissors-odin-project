bc4f61c891387143ad5a9c835cec6f54f2ec29a0
Pedro Ferragut <75221107+peferragut@users.noreply.github.com>
2023-02-08 10:45:04 -0300

    Create the function game that returns who won and the score

--- script.js
+++ script.js
@@ -10,27 +10,55 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "ROCK") {
         if (computerSelection === "PAPER") {
-            return "You Lose! Paper beats Rock";
+            return -1
         } else if (computerSelection === 'SCISSOR') {
-            return "You win! Rock beats Scissor";
+            return 1;
         } else {
-            return "It's a draw!";
+            return 0;
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "SCISSOR") {
-            return "You Lose! Scissor beats Paper";
+            return -1;
         } else if (computerSelection === "ROCK") {
-            return "You win! Paper beats Rock";
+            return 1;
         } else {
-            return "It's a draw!";
+            return 0;
         }
     } else {
         if (computerSelection === "ROCK") {
-            return "You Lose! Rock beats Scissor";
+            return -1;
         } else if (computerSelection === "PAPER") {
-            return "You win! Scissor beats Paper";
+            return 1;
         } else {
-            return "It's a draw!";
+            return 0;
         }
+    }
+}
+
+function game() {
+    let computerScore = 0, playerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Choose beween Rock, Paper and Scissor: ");
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
+        computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection);
+        if (result > 0) {
+            playerScore++;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        } else if (result < 0) {
+            computerScore++;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        } else {
+            console.log("It's a draw!")
+        }
+    }
+    console.log(`The final score is:
+Player ${playerScore} X ${computerScore} Computer`);
+    if (playerScore > computerScore) {
+        console.log("You won!");
+    } else if (playerScore < computerScore) {
+        console.log("You lost!");
+    } else {
+        console.log("It's a tie!");
     }
 }
